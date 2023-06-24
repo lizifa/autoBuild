69f80700faff0cf988a651acea41172574cfc12d
小书包 <550083126@qq.com>
2023-06-24 20:58:00 +0800

    feat: test

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const Koa = require('koa');
+const Router = require('koa-router');
+const port = 8888
+const app = new Koa();
+const router = new Router();
+
+router.get('/', async (ctx) => {
+    ctx.type = 'html';
+    ctx.body = '<h1>hello world!</h1>';
+})
+
+app.use(router.routes());
+app.use(router.allowedMethods({
+
+}));
+
+app.listen(port, () => {
+    console.log(`应用已经启动，http://localhost:${port}`);
+})
